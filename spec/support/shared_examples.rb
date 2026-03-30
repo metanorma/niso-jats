20a84bde9a93143a891294ba287eef30ffb54ec7
Ronald Tse <ronald.tse@ribose.com>
2026-03-30 19:09:17 +0800

    chore: add c14n preprocessing to XML test for canon bug

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -4,6 +4,8 @@ RSpec.shared_examples "a serializer" do
     serialized = described_class.from_xml(input)
     output = serialized.to_xml(declaration: true, encoding: "utf-8")
 
+    # TODO: The preprocessing is needed for Canon to ignore insignificant
+    # whitespace. We need to fix it.
     expect(output).to be_xml_equivalent_to(input, preprocessing: :c14n)
   end
 end
